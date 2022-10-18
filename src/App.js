5014050c8ea3f10365370acf4781dadaa345501f
Sudhansh Pal <sudpal1@Sudhanshs-MacBook-Pro.local>
2022-10-18 19:00:12 +0530

    header section

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import PokemonList from './components/PokemonList';
 function App() {
   return (
     <>
-      <div className='container'>
+      <div className='container-fluid h-100 mx-5'>
         <HeaderSection />
         <FilterSection />
         <PokemonList />
--- src/components/HeaderSection.js
+++ src/components/HeaderSection.js
@@ -1,10 +1,21 @@
-
-import './HeaderSection.scss';
+import "./HeaderSection.scss";
 
 function HeaderSection() {
   return (
     <>
-    <p>Header</p>
+      <h2 className="d-inline p-2" aria-label="Pokedex">
+        Pokedex
+      </h2>
+      <div className="d-none d-md-inline">
+        <div class="vr"></div>
+      </div>
+      <hr className="d-block d-md-none" />
+      <label
+        className="p-2"
+        aria-label="Search for any Pokemon that exists on the planet"
+      >
+        Search for any Pokemon that exists on the planet
+      </label>
     </>
   );
 }
