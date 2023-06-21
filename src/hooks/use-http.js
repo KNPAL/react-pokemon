63570e2a990632cb2d299739b6c0aaf7c5428633
Sudhansh Pal <sudpal1@Sudhanshs-MacBook-Pro.local>
2023-06-21 11:41:56 +0530

    update hook

--- src/hooks/use-http.js
+++ src/hooks/use-http.js
@@ -1,36 +1,33 @@
-import { useCallback, useState } from "react";
+import { useState } from "react";
 
 const useHttp = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
+  const [data, setData] = useState(null);
 
-  const sendRequest = useCallback(async (requestConfig,applyData) => {
+  const sendResponse = async (url) => {
     setIsLoading(true);
     setError(null);
     try {
-      const response = await fetch(requestConfig.url, {
-        method: requestConfig.method ? requestConfig.method : "GET",
-        Headers: requestConfig.Headers ? requestConfig.Headers : {},
-        body: JSON.stringify(requestConfig.body)
-          ? JSON.stringify(requestConfig.body)
-          : null,
-      });
+      const response = await fetch(url);
       if (!response.ok) {
-        throw new Error("Request failed!");
+        throw new Error("API request failed");
       }
-
-      const data = await response.json();
-      applyData(data);
-    } catch (err) {
-      setError(err.message || "Something went wrong");
+      const jsonData = await response.json();
+      console.log(jsonData)
+      setData(jsonData);
+      setIsLoading(false);
+    } catch (error) {
+      setError(error.message);
+      setIsLoading(false);
     }
-    setIsLoading(false);
-  },[]);
+  };
 
   return {
     isLoading,
     error,
-    sendRequest,
+    data,
+    sendResponse
   };
 };
 
